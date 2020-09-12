f50709b224f7175e98d023bf2b3879c1b05316bb
bashayerkq <70640706+bashayerkq@users.noreply.github.com>
2020-09-13 00:11:27 +0300

    done

---
+++ day2 -CW/cw2-p2-.MyPlayground.playground/Contents.swift
@@ -0,0 +1,14 @@
+var fruits = ["mango", "orange", "Potato", "Peach"]
+print(fruits[0])
+print(fruits[3])
+print(fruits)
+fruits.append("Strawberry")
+print(fruits)
+fruits.remove(at: 2)
+print(fruits)
+fruits += ["kiwi", "watermelon" , "banana" ]
+print(fruits)
+fruits.removeAll()
+
+
+
---
+++ day2 -CW/ios-cw2-p1.MyPlayground.playground/Contents.swift
@@ -0,0 +1,9 @@
+let family = ["mashael", "waleed", "shaikha", "hessa","bashayer"]
+var evens = [2,4,6,8,10,12]
+var grades = [96.1, 97.2, 98.3]
+var trueFalse = [true, true, true, false]
+print("Family:" , family[0], family[4])
+print("evens:" , evens[0], evens[5])
+print("grades" , grades[0], grades[2])
+print("TrueFalse" , trueFalse[0], trueFalse[3])
+
